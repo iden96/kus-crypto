d22ec47683384f4b1faf372dbf639716fabbe332
Denis Krugly <d.krugly69@gmail.com>
2020-10-24 22:31:25 +0300

    [feature] Add Block class and some unit tests

---
+++ src/classes/Block.class.ts
@@ -0,0 +1,39 @@
+import { SHA256 } from 'crypto-js';
+
+export default class Block {
+  timestamp: number;
+  lastHash: string;
+  hash: string;
+  data: object;
+
+  static genesis() {
+    return new this(23433432, '-----', 'f1r57-h45h', []);
+  }
+
+  static mineBlock(lastBlock: Block, data: object) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+    return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp: number, lastHash: string, data: object) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
+
+  constructor(timestamp: number, lastHash: string, hash: string, data: object) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  toString() {
+    return `Block -
+      Timestamp: ${this.timestamp}
+      Last Hash: ${this.lastHash.substring(0, 10)}
+      Hash     : ${this.hash.substring(0, 10)}
+      Data     : ${this.data}
+    `;
+  }
+}
---
+++ tests/block.test.ts
@@ -0,0 +1,21 @@
+import Block from '../src/classes/Block.class';
+
+describe('Block', () => {
+  let data: string;
+  let lastBlock: Block;
+  let block: Block;
+
+  beforeEach(() => {
+    data = [];
+    lastBlock = Block.genesis();
+    block = Block.mineBlock(lastBlock, data);
+  });
+
+  it('sets the `data` to match the input', () => {
+    expect(block.data).toEqual(data);
+  });
+
+  it('sets the `lastHash` to match the hash of the last block', () => {
+    expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+});
